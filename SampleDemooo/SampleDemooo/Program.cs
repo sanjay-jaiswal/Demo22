6818c755000b5dfca38b80778eeadeaf01ce6dcc
sanjay jaiswal <sanjujaiswal.dev@gmail.com>
2021-03-28 12:26:28 +0530

    pushed on uc1

--- SampleDemooo/SampleDemooo/Program.cs
+++ SampleDemooo/SampleDemooo/Program.cs
@@ -7,6 +7,9 @@ namespace SampleDemooo
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Console.WriteLine("This is uc1 branch ");
+
         }
     }
 }
